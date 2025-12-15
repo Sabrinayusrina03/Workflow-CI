67e5aa5533557c4b83e5d56cc4556f85bfaf55a5
Sabrinayusrina03 <syusrina89@gmail.com>
2025-12-16 01:22:16 +0700

    Fix MLflow start_run conflict in tuning

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -40,29 +40,28 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.set_experiment("Laptop Price Prediction - Tuning")
 
-with mlflow.start_run():
-    grid = GridSearchCV(
-        pipeline,
-        param_grid,
-        cv=3,
-        scoring="r2"
-    )
-    
-    grid.fit(X_train, y_train)
-    best_model = grid.best_estimator_
+grid = GridSearchCV(
+    pipeline,
+    param_grid,
+    cv=3,
+    scoring="r2"
+)
 
-    y_pred = best_model.predict(X_test)
+grid.fit(X_train, y_train)
+best_model = grid.best_estimator_
 
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+y_pred = best_model.predict(X_test)
 
-    # Manual logging
-    mlflow.log_param("alpha", grid.best_params_["model__alpha"])
-    mlflow.log_metric("MAE", mae)
-    mlflow.log_metric("R2", r2)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-    mlflow.sklearn.log_model(best_model, "model")
+# Manual logging (AMAN)
+mlflow.log_param("alpha", grid.best_params_["model__alpha"])
+mlflow.log_metric("MAE", mae)
+mlflow.log_metric("R2", r2)
 
-    print("Best alpha:", grid.best_params_)
-    print("MAE:", mae)
-    print("R2:", r2)
+mlflow.sklearn.log_model(best_model, "model")
+
+print("Best alpha:", grid.best_params_)
+print("MAE:", mae)
+print("R2:", r2)
