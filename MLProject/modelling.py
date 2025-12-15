eea5cde168746da9b09f3e84a884f1be2280d3b5
Sabrinayusrina03 <syusrina89@gmail.com>
2025-12-16 01:53:35 +0700

    Fix MLflow Projects run binding

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,7 +1,7 @@
+import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -9,7 +9,9 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_absolute_error, r2_score
 
-# Load data
+# 🔒 IKAT RUN DARI MLFLOW PROJECTS (PENTING)
+mlflow.start_run(run_id=os.environ.get("MLFLOW_RUN_ID"))
+
 df = pd.read_csv("laptop_clean.csv")
 
 X = df.drop(columns=["Price_euros"])
@@ -51,7 +53,6 @@ y_pred = best_model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# Logging (AMAN di MLflow Projects)
 mlflow.log_param("alpha", grid.best_params_["model__alpha"])
 mlflow.log_metric("MAE", mae)
 mlflow.log_metric("R2", r2)
